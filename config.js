adcc0117411ca7f7e76a837f2d39cc38816b21aa
Phathum <pathumnishshanka892@gmail.com>
2026-04-08 19:09:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SURBiKDJ#Klw3pmahHbQDs0c0dnbBoBX3CIjqk2gWNhFSBKOrPlw",
 AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
 ANTI_BAD_WORD: process.env.ANTI_BAD_WORD || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
